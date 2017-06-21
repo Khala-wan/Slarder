c96cbf256bd036d995b4e635ea94098df5d85658
wansheng <wansheng_ios@sina.com>
2017-06-21 16:58:48 +0800

    add source

---
+++ Slarder/Source/SlarderError.swift
@@ -0,0 +1,13 @@
+//
+//  SlarderError.swift
+//  PodMan
+//
+//  Created by 万圣 on 2017/6/20.
+//  Copyright © 2017年 万圣. All rights reserved.
+//
+
+import Foundation
+
+enum SlarderError : Error{
+    case ScriptNotFound(description:String)
+}
